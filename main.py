b76b9a7c8b21e73046de1f9a1a875d7b1ac50b1a
Kent <kentsmba@Kents-MacBook-Air.local>
2023-05-08 15:08:46 +0800

    add error handling, add action.yml

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
 from utils import insert_urls, update_meme, is_urls_empty, fetch_memes
+
+limit = sys.argv[1]
 
 # Fetching and insertion is available only if the comment block is empty
 if is_urls_empty():
-    insert_urls(fetch_memes())
+    insert_urls(fetch_memes(limit))
 
 update_meme()
--- utils.py
+++ utils.py
@@ -8,6 +8,8 @@ readme = os.path.abspath("README.md")
 
 def update_meme():
     start, _ = comment_area()
+    if not start:
+        return
 
     with open(readme, "r", encoding='utf-8') as md:
         lines = md.readlines()
@@ -25,21 +27,23 @@ def update_meme():
         rd.writelines(lines)
 
 
-def comment_area():
+def comment_area() -> tuple[int|None, int|None]:
     with open(readme, "r", encoding="utf-8") as rd:
         lines = rd.readlines()
 
     # Find the start line of the meme comment block
+    startline, endline = None,None
     for i, line in enumerate(lines):
         if "<!--MEME" in line:
             startline = i
             break
 
     # Find the end line of the meme comment block
-    for i, line in enumerate(lines[startline:]):
-        if line.strip() == "-->":
-            endline = startline + i
-            break
+    if startline:
+        for i, line in enumerate(lines[startline:]):
+            if line.strip() == "-->":
+                endline = startline + i
+                break
 
     return startline, endline
 
@@ -58,7 +62,8 @@ def insert_urls(data):
     startline, _ = comment_area()
     with open(readme, "r", encoding='utf-8') as rd:
         lines = rd.readlines()
-        lines = lines[:startline+1] + images + lines[startline+1:]
+        if startline:
+            lines = lines[:startline+1] + images + lines[startline+1:]
 
     with open(readme, 'w+', encoding='utf-8') as md:
         md.writelines(lines)
@@ -66,17 +71,21 @@ def insert_urls(data):
 
 def is_urls_empty():
     start, end = comment_area()
-    return start + 1 == end
+    if start and end:
+        return start + 1 == end
+    else:
+        raise Exception("Can't find the MEME mark in the comment")
 
 
-def fetch_memes():
-    # TODO: pass argument to set the amount
-
-    # I set the amount to 100 per fetch
-    url = "https://vvgskppmennronkqbstj.supabase.co/rest/v1/memes?select=*&offset=0&limit=100&order=id.asc"
+def fetch_memes(limit:int|str = 100):
+    # the amount is 100 per fetch by default
+    url = f"https://vvgskppmennronkqbstj.supabase.co/rest/v1/memes?select=*&offset=0&limit={limit}&order=id.asc"
 
     load_dotenv()
-    headers = {"apikey": os.getenv("apikey")}
+    if apikey := os.getenv("apikey"):
+        headers = {"apikey": apikey}
+    else:
+        raise Exception("apikey not found")
 
     response = requests.get(url, headers=headers)
     data = response.json()
